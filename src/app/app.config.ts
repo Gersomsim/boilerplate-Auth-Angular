259d480f2a19a35fbc253c60c7ae831627655b9f
Gersomsim <gersomhernandez@me.com>
2025-07-17 14:36:44 -0600

    ✨Feat: integrar Tailwind CSS y ngx-toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router'
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { DI_PROVIDER } from '@infrastructure/di'
 import { addTokenInterceptor, launchNotificationInterceptor } from '@infrastructure/interceptors'
+import { provideToastr } from 'ngx-toastr'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
+		provideToastr(),
+		provideAnimations(),
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
