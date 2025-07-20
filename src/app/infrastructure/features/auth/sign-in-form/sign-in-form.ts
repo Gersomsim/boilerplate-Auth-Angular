da8e530b70fc11d0bed3038086485999c514d000
Gersomsim <gersomhernandez@me.com>
2025-07-20 00:56:20 -0600

    ✨Feat: mejorar formulario de inicio de sesión y gestión de estado

--- src/app/infrastructure/features/auth/sign-in-form/sign-in-form.ts
+++ src/app/infrastructure/features/auth/sign-in-form/sign-in-form.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 import { Notification } from '@infrastructure/libraries/notification.library'
+import { AuthFacade } from '@infrastructure/states/facades/auth/auth.facade'
 import { UiModule } from '@infrastructure/ui/ui.module'
 import { ForgotPasswordLink } from '../forgot-password/forgot-password-link/forgot-password-link'
 
@@ -8,21 +10,31 @@ import { ForgotPasswordLink } from '../forgot-password/forgot-password-link/forg
 	selector: 'app-sign-in-form',
 	imports: [UiModule, ReactiveFormsModule, ForgotPasswordLink],
 	templateUrl: './sign-in-form.html',
-	styleUrl: './sign-in-form.scss',
 })
 export class SignInForm {
+	authFacade = inject(AuthFacade)
+	router = inject(Router)
 	fb = inject(FormBuilder)
 	notify = inject(Notification)
-	form = this.fb.group({
+	form = this.fb.nonNullable.group({
 		email: ['', [Validators.required, Validators.email]],
 		password: ['', [Validators.required, Validators.minLength(8)]],
 	})
+	isSubmitting = signal(false)
 
-	submit() {
+	async submit() {
 		if (this.form.invalid) {
 			this.form.markAllAsTouched()
 			this.notify.showWarning('Please fill in all fields')
 			return
 		}
+		this.isSubmitting.set(true)
+		await this.authFacade.signIn(this.form.getRawValue())
+		this.form.reset()
+		this.isSubmitting.set(false)
+		this.navigateToApp()
+	}
+	navigateToApp() {
+		this.router.navigate(['/app'])
 	}
 }
